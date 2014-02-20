47f0ed1c0d8fa609544c87b2b94faa0a1376a428
Alan Meert <ameert@chitou.physics.upenn.edu>
2014-02-20 10:45:52 -0500

    weighs top now, but over-weights top

--- AI/AI_shell.py
+++ AI/AI_shell.py
@@ -8,6 +8,12 @@ from config import *
 from kismet_scorecard import *
 from make_transition_table import get_prob
 
+hold_hands = ['ones','twos','threes','fours','fives','sixes',
+              'tp_sc','three_kind','straight','flush','full_house',
+              'fh_sc','four_kind','scar','kismet']
+tmp_scorecard = scorecard('tmp')
+hold_hand_rowvals = [tmp_scorecard.scores[a].rownum for a in hold_hands]
+del tmp_scorecard
 
 transition_info = np.load('../AI/transition_info.npz')
 #contains [dice_combos, transition_matrix]
@@ -17,9 +23,8 @@ transition_matrix = transition_info['transition_matrix']
 
 class score_array():
     def __init__(self):
-        self.hands = ['ones','twos','threes','fours','fives','sixes',
-                      'tp_sc','three_kind','straight','flush','full_house',
-                      'fh_sc','four_kind','scar','kismet']
+        self.hands = hold_hands
+        self.hand_rowvals = hold_hand_rowvals
         
         self.roll1_score = np.matrix(np.zeros((dice_combos.shape[0],len(self.hands))))
         self.roll23_score = np.matrix(np.zeros((dice_combos.shape[0],len(self.hands))))
@@ -48,22 +53,27 @@ class score_array():
             self.bonus_adjust = np.array(self.roll23_score)*np.array(self.score_adjust)
         
         top_score = scorecard.scores['top_total'].rowscore- scorecard.scores['top_bonus'].rowscore
-        print self.bonus_adjust[300:,:6]
-        print self.roll23_score[300:,:6]
-        a = self.bonus_adjust/(63.-top_score)
-        a = np.where(a>1., 1., a)
-        a = np.where(a<0., 0., a)
-        b = self.bonus_adjust/(61.-top_score)
-        b = np.where(b>1., 1., a)
-        b = np.where(b<0., 0., a)
-        c = self.bonus_adjust/(78.-top_score)
-        c = np.where(c>1., 1., a)
-        c = np.where(c<0., 0., a)
 
-        self.bonus_adjust = a*35 + b*(55-35) +c*(75-55)
+        #average_top_score = self.score_adjust*3.*np.arange(1,self.score_adjust.shape[1]+1) # 3 of each dice
+        #print average_top_score
+        if top_score<63:
+            a = self.bonus_adjust/(63.-top_score)
+            a = np.where(a>1., 1., a)
+        else:
+            a = 0*self.bonus_adjust
+        if top_score<71:
+            b = self.bonus_adjust/(71.-top_score)
+            b = np.where(b>1., 1., a)
+        else:
+            b = 0*self.bonus_adjust
+        if top_score<78:
+            c = self.bonus_adjust/(78.-top_score)
+            c = np.where(c>1., 1., a)
+        else:
+            c = 0*self.bonus_adjust
+       
+        self.bonus_adjust = (a*35 + b*(55-35) +c*(75-55))#-average_top_score*75.0/78.0*np.array(self.score_adjust)
 
-        print dice_combos[300:]
-        print self.bonus_adjust[300:,:6]
         return self.open_scores, self.bonus_adjust
 
 
@@ -82,38 +92,49 @@ def AI_choose_option(options, scorecard, rollnum, hand_dice):
     test_score = score_array()
     test_score.update_open_scores(scorecard, rollnum)
 
-    out_dice = []
-    for count in [17,15,14,13,12,11]:
-        if count in options.keys():
-            if options[count][2]>0:
-                choice = count
-                out_dice = hand_dice
-                break
-    else:
+    out_dice = hand_dice
+    hand = [a.number for a in hand_dice]
+            
+    if len(options.keys())>1:
+        curr_hand_choice = np.where(choice_hands(hand, 3)==1)[0] #use roll 3 to check current hand
+        print curr_hand_choice, ' currchoice'
+
+        if rollnum == 1:
+            tmp_score = test_score.roll1_score+test_score.bonus_adjust
+        else:
+            tmp_score = test_score.roll23_score+test_score.bonus_adjust
+        curr_scores = (np.array(tmp_score[curr_hand_choice,:])*np.array(test_score.open_scores))[0,:]
+
+        print curr_scores
+        stop_choice = np.argsort(curr_scores)[-1]
+        stop_score = curr_scores[stop_choice]
+        choice =  test_score.hand_rowvals[stop_choice]
+        print 'present ',stop_choice, stop_score, choice
+
         if 999 in options.keys():
-            choice = 999
-            hand = [a.number for a in hand_dice]
             choice_arr =choice_hands(hand, rollnum) 
             curr_trans_arr = (choice_arr*transition_matrix.T).T
-            future_score = np.array(curr_trans_arr*test_score.roll23_score)*np.array(test_score.open_scores)
-            future_choice = np.sum(future_score, axis =1)
-            best_hand = np.where(future_choice == np.max(future_choice))[0]
-            best_hand = np.array(dice_combos[best_hand]).flatten()
-            hand_dice_copy = [a for a in hand_dice]
-            print 'best_hand ',best_hand
-            for a in best_hand:
-                for count, b in enumerate(hand_dice_copy):
-                    if a == b.number:
-                        out_dice.append(hand_dice_copy.pop(count))
-                        break
-            
-        else: 
-            best = (-1,-1)
-            for a in options.items():
-                if a[1][2] > best[1]:
-                    best = (a[0], a[1][2])
-            choice = best[0]
-            out_dice = hand_dice
+            future_all_score = np.array(curr_trans_arr*test_score.roll23_score)*np.array(test_score.open_scores)
+            future_score = np.sum(future_all_score, axis =1)
+            future_choice = np.where(future_score == np.max(future_score))[0]
+            future_score = np.max(future_all_score[future_choice])
+            future_all_score[future_choice]
+            print 'future ',future_choice, future_score
+
+            if future_score>stop_score:
+                out_dice = []
+                choice = 999
+                best_hand = np.array(dice_combos[future_choice]).flatten()
+                hand_dice_copy = [a for a in hand_dice]
+                print 'best_hand ',best_hand
+                for a in best_hand:
+                    for count, b in enumerate(hand_dice_copy):
+                        if a == b.number:
+                            out_dice.append(hand_dice_copy.pop(count))
+                            break
+
+    else: # if there is only one choice
+        choice = options.keys()[0]
 
     return choice, out_dice
 
